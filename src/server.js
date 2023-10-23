4618fdd971012d6aed2278de0afd60e5cba81e30
invokerphytone <invokerphytone@gmail.com>
2023-10-23 20:08:00 +0700

    patch 4

--- src/server.js
+++ src/server.js
@@ -1,18 +1,25 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
+
 const init = async () => {
     const server = Hapi.server({
         port: 80,
         host: process.argv.includes('--host') ? process.argv[process.argv.indexOf('--host') + 1] : 'localhost',
-        routes:{
-            cors:{
-                origin:["*"],
+        routes: {
+            cors: {
+                origin: ["*"],
             },
         },
     });
+
     server.route(routes);
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+
+    try {
+        await server.start();
+        console.log(`Server berjalan pada ${server.info.uri}`);
+    } catch (error) {
+        console.error('Error starting the server:', error);
+    }
 };
 
 init();
